1cb26c338c6aa1cf5f4e009f73b3c166b4f6b4ab
khaled <5aledmo7sn@gmail.com>
2017-10-08 15:14:36 +0200

    first commit

---
+++ 1-Run first app in java/src/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// code.................
+		
+		//output coode :D 
+		System.out.println("Hellow World I'm in Here");
+		
+	}
+
+}
---
+++ Variable app 1/src/variableapp/Main.java
@@ -0,0 +1,31 @@
+package variableapp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		//Variable 
+		int x = 10 ; 
+		int y = 5 ; 
+		int z ;
+		z=x+y ;
+	
+		System.out.println("The result of the ( collection + ) process = "+z);
+		
+		z=x-y ;
+		
+		System.out.println("The result of the ( subtraction - ) process = "+z);
+		
+		z=x*y ;
+		
+		System.out.println("The result of the ( multiplication * ) process = "+z);
+		
+		z=x/y ;
+		
+		System.out.println("The result of the ( Divide / ) process = "+z);
+		
+		
+	}
+
+}
